45328f4f728ae0179d3aad61c00f665bb6b7dbd3
abdualla1 <38115506+abdualla1@users.noreply.github.com>
2024-03-20 21:12:41 +0200

    setup
    added files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "Image_Class.h"
+
+#define endl '\n'
+#define ll long long
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+signed main(){
+    fast
+
+    return 0;
+}

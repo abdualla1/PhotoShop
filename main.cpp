96cc529fd6606d6c3a3cbbff30b9e255779bfe66
abdualla1 <38115506+abdualla1@users.noreply.github.com>
2024-03-21 00:03:01 +0200

    tt
    tt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include "Image_Class.h"
 using namespace std;
 #define endl '\n'
 #define ll long long
 #define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void test(){
+    Image img("test.jpg");
+    img.saveImage("test.png");
 
+<<<<<<< Updated upstream
+}
+=======
+void 
+>>>>>>> Stashed changes
 int main() {
     fast
 
